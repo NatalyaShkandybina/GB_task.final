b7220b6509018f5b41a82c1365c28a149c4a65e2
Natalya Shkandybina <natalya.shkandybina@mail.ru>
2022-09-11 09:22:32 +0300

    info about void ЗPrintArray

--- task_final/Program.cs
+++ task_final/Program.cs
@@ -11,19 +11,6 @@
 //[“Russia”, “Denmark”, “Kazan”] → []
 string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length];
-// Задача: Написать программу, которая из имеющегося массива строк
- //формирует новый массив из строк, длина которых меньше,
-  //либо равна 3 символам. 
-  //Первоначальный массив можно ввести с клавиатуры, 
-  //либо задать на старте выполнения алгоритма. 
-  //При решении не рекомендуется пользоваться коллекциями, 
-//лучше обойтись исключительно массивами.
-//Примеры:
-//[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-//[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-//[“Russia”, “Denmark”, “Kazan”] → []
-string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
-string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
@@ -35,3 +22,14 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         count++;
         }
     }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
